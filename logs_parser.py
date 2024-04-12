5a40f68d1b5b63e60cc85b65e5dc5a4709b28eac
Daniel Laszlo <daniel@serket-tech.com>
2024-04-12 19:08:38 +0200

    Update logs visualizer

--- logs_parser.py
+++ logs_parser.py
@@ -1,12 +1,18 @@
 from pathlib import Path
+from dataclasses import dataclass
 import jsonpickle
 import json
+from io import StringIO
+
+from collections import Counter, defaultdict
 
 from typing import Dict, List
+import pandas as pd
 
 from icecream import ic
 
 from visualize import Order, create_order_grid, order_visualizer
+
 import pyvista as pv
 
 import numpy as np
@@ -19,78 +25,138 @@ SECTIONS = [
 ]
 
 SUBMISSIONS = Path("submissions")
+BACKTESTS = Path("backtests")
 
-submission = SUBMISSIONS / "23566cae-8e27-47fe-86da-42d7df0daf25"
+
+submission = sorted(list(BACKTESTS.iterdir()))[-1]
+
+## this is the result of mine
+# submission = SUBMISSIONS / "67e76d9f-f4ce-42dd-a82d-2c47f5c98f62"
+
+## this is the result of Alvise
+submission = SUBMISSIONS / "c1ec518f-ca42-4fbf-b607-a848b7aa48ef"
 
 
-csv = submission.with_suffix(".csv")
+## 
+
+# submission = BACKTESTS / "2024-04-12_03-59-26"
+
+submission = BACKTESTS / "2024-04-12_04-42-46"
+
+## mix submission
+submission = SUBMISSIONS / "a36e6a41-6a19-4335-a273-701902790d9f"
+
+# BACKTESTS / 
+
+print(submission)
+
+# PRODUCT = "AMETHYSTS"
+PRODUCT = "STARFRUIT"
+
+
+# csv = submission.with_suffix(".csv")
+# orders_df = pd.read_csv(csv, sep=";")
+
 log = submission.with_suffix(".log")
 
 
-if __name__ == "__main__":
-    log_file = Path("logs/no_trade_log_dump.log")
+# order_visualizer(bids_time2orders, asks_time2orders)
 
-    log_file = Path("logs/day1_no_trade_dump.log")
-    with log_file.open() as f:
-        contents = f.read()
 
-    remaining = contents
-    pre, separator, remaining = remaining.partition("Sandbox logs:")
-    sandbox_logs, separator, remaining = remaining.partition("Activities log:")
-    activities_log, separator, remaining = remaining.partition("Trade History:")
-    trade_history = remaining
+# ###########################################
+# Parse the df into bid and ask orders
+# ###########################################
+with log.open() as f:
+    contents = f.read()
 
-    def parse_next_sandbox_log(contents: str):
-        next, separator, remaining = contents.partition("\n{")
-        # wtf = "}"
-        return next, remaining
+remaining = contents
+pre, separator, remaining = remaining.partition("Sandbox logs:")
+sandbox_logs, separator, remaining = remaining.partition("Activities log:")
+activities_log, separator, remaining = remaining.partition("Trade History:")
+trade_history = remaining
 
-    empty, remaining = parse_next_sandbox_log(sandbox_logs)
 
-    parsed_logs = []
-    while remaining:
-        next_log, remaining = parse_next_sandbox_log(remaining)
-        f = eval(f"{{{next_log}")  # }}
-        log_str = f["lambdaLog"]
-        try:
-            log = eval(log_str)
-            parsed_logs.append(log)
-        except SyntaxError:
-            print(f"Could not parse log: {log_str}")
+orders_df = pd.read_csv(StringIO(activities_log), sep=";")
 
-    log = parsed_logs[0]
-    ic(log)
+# ###########################################
+# Parse the df into bid and ask orders
+# ###########################################
+bids_time2orders = {}
+asks_time2orders = {}
+for i, row in orders_df.iterrows():
+    if row["product"] != PRODUCT:
+        continue
+    #
+    timestamp = row["timestamp"]
+    bids = []
+    asks = []
+    #
+    for idx in [1, 2, 3]:
+        bid_price = row[f"bid_price_{idx}"]
+        bid_volume = row[f"bid_volume_{idx}"]
+        ask_price = row[f"ask_price_{idx}"]
+        ask_volume = row[f"ask_volume_{idx}"]
+        # check for nan
+        if not np.isnan(bid_price):
+            bids.append(Order(price=bid_price, volume=bid_volume))
+            asks.append(Order(price=ask_price, volume=ask_volume))
+    #
+    bids_time2orders[timestamp] = bids
+    asks_time2orders[timestamp] = asks
 
-    # %%
-    # Visualization starts here
 
-    buy_time2orders = {}
-    sell_time2orders = {}
-    timestamps = []
-    prices = []
-    volumes = []
-    SAMPLE_N = 1000
-    SAMPLE_N = None
-    for log in parsed_logs[:SAMPLE_N]:
-        # PRODUCT = "AMETHYSTS"
-        PRODUCT = "STARFRUIT"
-        buy_orders = log["order_depths"][PRODUCT]["buy_orders"]
-        sell_orders = log["order_depths"][PRODUCT]["sell_orders"]
-        timestamp = log["timestamp"]
-        timestamps.append(timestamp)
-        buy_order_objects = []
-        for price, volume in buy_orders.items():
-            buy_order_objects.append(Order(price=price, volume=volume))
-            prices.append(int(price))
-            volumes.append(volume)
-        #
-        sell_order_objects = []
-        for price, volume in sell_orders.items():
-            sell_order_objects.append(Order(price=price, volume=volume))
-            prices.append(int(price))
-            volumes.append(volume)
-        #
-        buy_time2orders[timestamp] = buy_order_objects
-        sell_time2orders[timestamp] = sell_order_objects
+history = eval(trade_history)
 
-    order_visualizer(buy_time2orders, sell_time2orders)
+
+@dataclass
+class Trade:
+    timestamp: int
+    buyer: str
+    seller: str
+    product: str
+    price: int
+    volume: int
+
+
+trades = [
+    Trade(
+        timestamp=h["timestamp"],
+        buyer=h["buyer"],
+        seller=h["seller"],
+        product=h["symbol"],
+        price=h["price"],
+        volume=h["quantity"],
+    )
+    for h in history
+]
+
+bot_trades = []
+own_trades = []
+
+for trade in trades:
+    if trade.buyer == "SUBMISSION" or trade.seller == "SUBMISSION":
+        own_trades.append(trade)
+    else:
+        bot_trades.append(trade)
+
+sellers = Counter([t.seller for t in trades])
+buyers = Counter([t.buyer for t in trades])
+
+own_trades = [t for t in own_trades if t.product == PRODUCT]
+# for trade in own_trades:
+#     if trade.buyer == "SUBMISSION":
+#         trade.volume = -trade.volume
+
+
+time2trades = defaultdict(list)
+
+for trade in own_trades:
+    time2trades[trade.timestamp].append(Order(price=trade.price, volume=trade.volume))
+
+# # are there 2 trades in any timestamp?
+# for timestamp, trades in time2trades.items():
+#     if len(trades) > 1:
+#         print(timestamp, trades)
+
+
+order_visualizer(bids_time2orders, asks_time2orders, time2trades)
--- visualize.py
+++ visualize.py
@@ -1,9 +1,14 @@
 from dataclasses import dataclass
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 import numpy as np
 import pyvista as pv
 from pyvista import CellType
+
+from statistics import NormalDist
+
+
+from distributions import NormalDistribution, Sample
 
 
 @dataclass
@@ -72,13 +77,17 @@ class OrderDepth:
 
 
 # def create_order_grid(timestep: int, orders: List[Order]):
-def create_order_grid(time2orders: Dict[int, List[Order]]):
+def create_order_grid(
+    time2orders: Dict[int, List[Order]], cell_size=0.7, extra_depth=0.0
+) -> pv.UnstructuredGrid:
     all_cells = []
     for timestep, orders in time2orders.items():
         cells = [
             Cell(
                 position=Point(x=timestep / 100, y=order.price, z=0),
-                size=Box(width=0.7, height=0.7, depth=order.volume),
+                size=Box(
+                    width=cell_size, height=cell_size, depth=order.volume + extra_depth
+                ),
             )
             for order in orders
         ]
@@ -87,11 +96,149 @@ def create_order_grid(time2orders: Dict[int, List[Order]]):
     return grid
 
 
+def sliding_windows(lst: list, window_size: int, step=1):
+    for i in range(window_size, len(lst), step):
+        yield lst[i - window_size : i]
+
+
+def create_probability_grid(time2orders: Dict[int, List[Order]]):
+    time_and_orders = list(time2orders.items())
+    time_and_orders.sort(key=lambda x: x[0])
+
+    time_discount = 0.8
+
+    for window in sliding_windows(time_and_orders, window_size=10):
+        current_factor = 1.0
+
+        samples = []
+        for timestep, orders in window[::-1]:
+            for order in orders:
+                samples.append(
+                    Sample(value=order.price, weight=current_factor * order.volume)
+                )
+            current_factor *= time_discount
+
+        # calculate normal distribution based on samples
+        dist = NormalDistribution.from_samples(samples)
+
+        _, current_orders = window[-1]
+        prices = np.array([order.price for order in current_orders])
+        price_range = (prices.min(), prices.max())
+
+        from visualize import draw_distribution
+
+
+@dataclass
+class Limits:
+    min: float
+    max: float
+
+
+def create_grid(
+    x: Limits, y: Limits, z: Limits, spacing: float = 10
+) -> pv.StructuredGrid:
+    xrng = np.arange(x.min, x.max + spacing, spacing, dtype=np.float32)
+    yrng = np.arange(y.min, y.max + spacing, spacing, dtype=np.float32)
+    zrng = np.arange(z.min, z.max + spacing, spacing, dtype=np.float32)
+    mx, my, mz = np.meshgrid(xrng, yrng, zrng, indexing="ij")
+    grid = pv.StructuredGrid(mx, my, mz)
+    return grid
+
+
+@dataclass
+class Toggle:
+    """Helper callback to keep a reference to the actor being modified."""
+
+    pl: pv.Plotter
+    actor: pv.PolyData
+    t: bool = True
+
+    def __call__(self, flag: Optional[bool] = None):
+        if flag:
+            self.t = flag
+        else:
+            self.t = not self.t
+        self.actor.SetVisibility(self.t)
+        self.pl.update(force_redraw=True)
+
+
+@dataclass
+class GridArgs:
+    grid: pv.UnstructuredGrid
+    color: Optional[str] = None
+    scalars: Optional[str] = None
+    show_edges: bool = True
+    style: str = "surface"
+    toggle_hotkey: Optional[str] = None
+
+
+def visualize_grids(grids: List[GridArgs]):
+    pv.set_plot_theme("dark")
+
+    pl = pv.Plotter()
+
+    for i, grid_args in enumerate(grids):
+        mesh = pl.add_mesh(
+            grid_args.grid,
+            color=grid_args.color,
+            scalars=grid_args.scalars,
+            style=grid_args.style,
+            show_edges=grid_args.show_edges,
+        )
+
+        toggle = Toggle(pl, mesh)
+
+        size = 20
+        padding = 5.0
+        pl.add_checkbox_button_widget(
+            toggle,
+            value=True,
+            position=(i * size + padding, padding),
+            size=20,
+            border_size=2,
+            color_on=grid_args.color if grid_args.color else "white",
+            color_off="gray",
+        )
+        if grid_args.toggle_hotkey:
+            pl.add_key_event(grid_args.toggle_hotkey, toggle)
+
+    pl.add_camera_orientation_widget(animate=True, n_frames=20)
+    pl.enable_parallel_projection()
+
+    # def cell_pick_callback(cells):
+    #     print()
+    #     print(cells)
+    #
+    # pl.enable_cell_picking(callback=cell_pick_callback)
+
+    # pl.show_grid(font_size=1, n_ylabels=2)
+
+    def set_camera_position(plane: str):
+        assert plane == "xy" or plane == "yz" or plane == "zx"
+        pl.camera_position = plane
+        pl.zoom_camera(8)
+
+    pl.add_key_event("x", lambda: set_camera_position("yz"))
+    pl.add_key_event("y", lambda: set_camera_position("zx"))
+    pl.add_key_event("z", lambda: set_camera_position("xy"))
+
+    pl.add_key_event("s", lambda: pl.screenshot("screenshot.png"))
+
+    pl.enable_fly_to_right_click()
+
+    pl.show()
+
+
 def order_visualizer(
-    buy_time2orders: Dict[int, List[Order]], sell_time2orders: Dict[int, List[Order]]
+    buy_time2orders: Dict[int, List[Order]],
+    sell_time2orders: Dict[int, List[Order]],
+    trades: Optional[Dict[int, List[Order]]],
 ):
     buy_grid = create_order_grid(buy_time2orders)
     sell_grid = create_order_grid(sell_time2orders)
+
+    if trades is not None:
+        trade_grid = create_order_grid(trades, cell_size=0.8, extra_depth=0.1)
 
     timestamps = list(buy_time2orders.keys())
     prices = []
@@ -115,64 +262,24 @@ def order_visualizer(
     prices = np.array(prices)
     volumes = np.array(volumes)
 
-    xrng = np.arange(timestep.min(), timestep.max() + 10, 10, dtype=np.float32)
-    yrng = np.arange(prices.min(), prices.max() + 10, 10, dtype=np.float32)
-    zrng = np.arange(volumes.min(), volumes.max() + 10, 10, dtype=np.float32)
-    x, y, z = np.meshgrid(xrng, yrng, zrng, indexing="ij")
+    xlimits = Limits(timestep.min(), timestep.max())
+    ylimits = Limits(prices.min(), prices.max())
+    zlimits = Limits(volumes.min(), volumes.max())
 
-    grid = pv.StructuredGrid(x, y, z)
+    grid = create_grid(xlimits, ylimits, zlimits)
 
-    pv.set_plot_theme("dark")
+    grids = [
+        GridArgs(grid, show_edges=True, style="wireframe", toggle_hotkey="g"),
+        GridArgs(buy_grid, color="green", toggle_hotkey="b"),
+        GridArgs(sell_grid, color="red", toggle_hotkey="a"),
+    ]
 
-    pl = pv.Plotter()
+    if trades is not None:
+        grids.append(GridArgs(trade_grid, color="blue", toggle_hotkey="t"))
 
-    buy_mesh = pl.add_mesh(buy_grid, color="green", show_edges=True)
-    sell_mesh = pl.add_mesh(sell_grid, color="red", show_edges=True)
+    visualize_grids(grids)
 
-    pl.add_mesh(grid, show_edges=True, style="wireframe")
-
-    class SetVisibilityCallback:
-        """Helper callback to keep a reference to the actor being modified."""
-
-        def __init__(self, actor):
-            self.actor = actor
-
-        def __call__(self, state):
-            self.actor.SetVisibility(state)
-
-    pl.add_checkbox_button_widget(
-        SetVisibilityCallback(buy_mesh),
-        value=True,
-        position=(5.0, 5.0),
-        size=20,
-        border_size=2,
-        color_on="green",
-        color_off="gray",
-    )
-
-    pl.add_checkbox_button_widget(
-        SetVisibilityCallback(sell_mesh),
-        value=True,
-        position=(25.0, 5.0),
-        size=20,
-        border_size=2,
-        color_on="red",
-        color_off="gray",
-    )
-
-    pl.add_camera_orientation_widget(animate=True, n_frames=20)
-    pl.enable_parallel_projection()
-
-    def set_camera_position(plane: str):
-        assert plane == "xy" or plane == "yz" or plane == "zx"
-        pl.camera_position = plane
-        pl.zoom_camera(8)
-
-    pl.add_key_event("x", lambda: set_camera_position("yz"))
-    pl.add_key_event("y", lambda: set_camera_position("zx"))
-    pl.add_key_event("z", lambda: set_camera_position("xy"))
-
-    pl.show()
+    return
 
 
 def test():
@@ -187,7 +294,7 @@ def test():
 if __name__ == "__main__":
     grids = []
     for timestep in range(10):
-        grid = create_order_grid(timestep, [Order(1, 1), Order(2, 2)])
+        grid = create_order_grid({timestep: [Order(1, 1), Order(2, 2)]})
         grids.append(grid)
 
     pl = pv.Plotter()
